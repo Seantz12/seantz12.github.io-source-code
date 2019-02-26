f2d98d1935ae3afbd5dcffdeb8d09d56e8879f0c
Sean Zhou <seantxzhou12@gmail.com>
2019-02-25 22:30:52 -0500

    Added in introduction text

---
+++ overlay.js
@@ -0,0 +1,13 @@
+function hide_overlay() {
+    element = document.getElementById("overlay")
+    var op = 1;
+    var timer = setInterval(function () {
+        if(op <= 0.1) {
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.2;
+    }, 50);
+}
