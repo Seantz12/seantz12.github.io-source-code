1e5d611ff1089e7d8d66827316d8ba6e2a5231ca
Sean Zhou <seantxzhou12@gmail.com>
2019-04-04 21:28:38 -0400

    Styling

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
-const BODY_DIV_ELEMENTS = ["homeText", "aboutText", "projectsText"];
-const NAV_DIV_ELEMENTS = ["about", "projects", "home"];
+const BODY_DIV_ELEMENTS = ['homeText', 'aboutText', 'projectsText'];
+const NAV_DIV_ELEMENTS = ['about', 'projects', 'home'];
 
 function leaveStartPage() {
     document.location.href = "about.html"
@@ -19,8 +19,8 @@ function fadeOutAndIn(fadeOutID, fadeInID) {
     var timer = setInterval(function () {
         if(op <= 0.1) {
             clearInterval(timer);
-            element.style.display = "none";
-            main.style.display = "block";
+            element.style.display = 'none';
+            main.style.display = 'block';
         }
         element.style.opacity = op;
         main.style.opacity = 1-op;
@@ -28,21 +28,21 @@ function fadeOutAndIn(fadeOutID, fadeInID) {
     }, 50);
 }
 
-function fadeOutThenIn(fadeOutId, fadeInId) {
+function fadeOutThenIn(fadeOutId, fadeInId, displayStyle="block") {
     element = document.getElementById(fadeOutId);
     var op = 1;
     var timer = setInterval(function () {
         if(op <= 0.1) {
             clearInterval(timer);
-            element.style.display = "none";
-            fadeIn(fadeInId);
+            element.style.display = 'none';
+            fadeIn(fadeInId, displayStyle);
         }
         element.style.opacity = op;
         op -= op * 0.2;
     }, 50);
 }
 
-function fadeIn(fadeInId) {
+function fadeIn(fadeInId, displayStyle="block") {
     element = document.getElementById(fadeInId);
     op = 1;
     element.style.opacity = 0;
@@ -50,7 +50,7 @@ function fadeIn(fadeInId) {
         if(op <= 0.1) {
             clearInterval(timer);
         } else if(op == 0.8) { // Fixes flashing
-            element.style.display = "block";
+            element.style.display = displayStyle;
         }
         element.style.opacity = 1-op;
         op -= op * 0.2;
@@ -60,16 +60,16 @@ function fadeIn(fadeInId) {
 function loadProjects() {
     for(var i = 0; i < BODY_DIV_ELEMENTS.length; i++) {
         var element = BODY_DIV_ELEMENTS[i];
-        if(element == "projectsText") {
+        if(element == 'projectsText') {
             continue;
         }
         var html_element = document.getElementById(element);
-        if(html_element.style.display == "block") {
-            fadeOutThenIn(element, "projectsText");
+        if(html_element.style.display == 'block') {
+            fadeOutThenIn(element, 'projectsText', "inline");
             break;
         }
     }
-    highlightElement("projects")
+    highlightElement('projects')
 }
 
 function loadAboutMe() {
@@ -79,12 +79,12 @@ function loadAboutMe() {
             continue;
         }
         var html_element = document.getElementById(element);
-        if(html_element.style.display == "block") {
-            fadeOutThenIn(element, "aboutText");
+        if(html_element.style.display == 'block') {
+            fadeOutThenIn(element, 'aboutText');
             break;
         }
     }
-    highlightElement("about")
+    highlightElement('about')
 }
 
 function loadHome() {
@@ -94,12 +94,12 @@ function loadHome() {
             continue;
         }
         var html_element = document.getElementById(element);
-        if(html_element.style.display == "block") {
-            fadeOutThenIn(element, "homeText");
+        if(html_element.style.display == 'block') {
+            fadeOutThenIn(element, 'homeText');
             break;
         }
     }
-    highlightElement("home")
+    highlightElement('home')
 }
 
 function highlightElement(element) {
