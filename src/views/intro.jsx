be856341e5b332cbb8a3147d0aca250e5b9105b7
Sean Zhou <seantxzhou12@gmail.com>
2019-11-04 21:25:56 -0500

    Add S.E.A.N method

--- src/views/intro.jsx
+++ src/views/intro.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Sean from '../components/sean.jsx';
 
 class IntroView extends React.Component {
     render() {
         return (
-            <div>Hello World!</div>
+            <Sean/>
         );
     }
 }
