5e955e10c7ccfb52e493ee5163919e8946c4c949
Sean Zhou <seantxzhou12@gmail.com>
2020-04-28 16:26:51 -0400

    add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Hello World!</p>
+    <Navbar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Navbar
   }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="navbar">
+        <p v-for="(link, key) in navLinks" :key="key">
+            {{key}}: {{link}}
+        </p>
+        <!-- <ul id="navlinks">
+            <li v-for="(link, key) in navLinks" :key="key">
+                {{key}}
+            </li>
+        </ul> -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "navbar",
+    data: function () {
+        return { 
+            navLinks: {
+                introduction: "#introduction",
+                something: "#something"
+            }
+        };
+    }
+}
+</script>
